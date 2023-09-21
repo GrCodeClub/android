83ee65c1a2dc2724ee298aa4f3251daead71015a
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-09-21 15:07:05 +0300

    Create menu.js

---
+++ post/menu.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var  menuLink = '<li><a href="https://grcodeclub.github.io">Αρχική Σελίδα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = ' <li><a href="https://grcodeclub.github.io/android">Android</a></li>';
+    $('.sidebarMenuInner').append(menuLinkAndroid);
+
+  });
+  
