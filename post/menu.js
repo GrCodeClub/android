0deac7ceb20b2ab64338fe32b89baf6102746ce6
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-09-22 15:56:58 +0300

    Update menu.js

--- post/menu.js
+++ post/menu.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     menuLink = ' <li><a href="https://grcodeclub.github.io/android">Android</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-    menuLink = ' <li><a href="https://grcodeclub.github.io/android/post/version.html">Eκδόσεις</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.github.io/android/post/versions.html">Eκδόσεις</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
   });
