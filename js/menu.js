44176017eee353091dfcf788268509fd58ca1c6c
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-02-10 20:34:09 +0200

    Update and rename menu.js to menu.js

--- js/menu.js
+++ js/menu.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     menuLink = ' <li><a href="https://grcodeclub.github.io/android">Android</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-    menuLink = ' <li><a href="https://grcodeclub.github.io/android/post/versions.html">Eκδόσεις</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.github.io/android/versions">Eκδόσεις</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
   });
